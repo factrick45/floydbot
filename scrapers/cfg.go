84b838cfa3ddeb83be62afa67c2ff567f6f7bf1d
IHM <118293336+factrick45@users.noreply.github.com>
2023-07-31 22:13:13 -0500

    literally one byte

--- scrapers/cfg.go
+++ scrapers/cfg.go
@@ -1,5 +1,5 @@
 package scrapers
 
 const CRAWL_RATE = 10
-const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
+const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
 	"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
